756485a4af7f3887f0136ea9bbd605838106d69e
Abdallah Abdelazim <abdallah.abdelazim@hotmail.com>
2020-12-05 20:06:26 +0200

    Add colored output in

--- upall.sh
+++ upall.sh
@@ -5,26 +5,31 @@
 # License: GNU General Public License v3.0
 #
 
+LIGHT_BLUE='\033[1;34m'
+GREEN='\033[0;32m'
+DARK_GREY='\033[1;30m'
+NC='\033[0m' # No color
+
 # [1] apt
-echo -e "--> Updating APT packages:"
+echo -e "${LIGHT_BLUE}Updating APT packages:${NC}"
 sudo apt update && sudo apt upgrade
-echo -e "Done."
-echo -e "\n--> Executing 'apt clean':"
+echo -e "${GREEN}Done.${NC}\n"
+echo -e "${LIGHT_BLUE}Executing 'apt clean':${NC}"
 sudo apt clean
-echo -e "Done."
-echo -e "\n--> Executing 'apt autoremove':"
+echo -e "${GREEN}Done.${NC}\n"
+echo -e "${LIGHT_BLUE}Executing 'apt autoremove':${NC}"
 sudo apt autoremove
-echo -e "Done.\n"
+echo -e "${GREEN}Done.${NC}\n"
 
 # [2] flatpak
-echo -e "--> Updating Flatpaks:"
+echo -e "${LIGHT_BLUE}Updating Flatpaks:${NC}"
 flatpak update
-echo -e "Done.\n"
+echo -e "${GREEN}Done.${NC}\n"
 
 # [3] snap
-echo -e "--> Updating Snaps:"
+echo -e "${LIGHT_BLUE}Updating Snaps:${NC}"
 sudo snap refresh
-echo -e "Done.\n"
+echo -e "${GREEN}Done.${NC}\n"
 
 
-echo -e "\aUPALL update script finished."
+echo -e "\a${DARK_GREY}UPALL update script finished.${NC}"
